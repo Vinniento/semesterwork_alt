f42fabf9c79607724f75651d3921117ad6b534e8
Lucas_NB\Lucas01 <lucas@thiess.info>
2019-04-17 18:17:20 +0200

    Fehlerbehebung + erweiterung[nicht fertig]

--- create_check.php
+++ create_check.php
@@ -1,48 +1,67 @@
 <?php
+
+//von https://www.geeksforgeeks.org/generating-random-string-using-php/
+function getName($n) { 
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+    $randomString = ''; 
+  
+    for ($i = 0; $i < $n; $i++) { 
+        $index = rand(0, strlen($characters) - 1); 
+        $randomString .= $characters[$index]; 
+    } 
+  
+    return $randomString; 
+} 
+
+
+
+
+
+
 $name=$_POST['name'];
-$criterion1 ;=NULL
+$criterion1 =NULL;
 if(isset($_POST['criterion 1'])
 {
 $criterion1 =$_POST['criterion 1'];
 }
 
-$criterion2 ;=NULL
+$criterion2 =NULL;
 if(isset($_POST['criterion 2'])
 {
 $criterion2 =$_POST['criterion 2'];
 }
 
-$criterion3 ;=NULL
+$criterion3 =NULL;
 if(isset($_POST['criterion 3'])
 {
 $criterion3 =$_POST['criterion 3'];
 }
 
-$criterion4 ;=NULL
+$criterion4 =NULL;
 if(isset($_POST['criterion 4'])
 {
 $criterion4 =$_POST['criterion 4'];
 }
 
-$criterion5 ;=NULL
+$criterion5 =NULL;
 if(isset($_POST['criterion 5'])
 {
 $criterion5 =$_POST['criterion 5'];
 }
 
-$criterion6 ;=NULL
+$criterion6 =NULL;
 if(isset($_POST['criterion 6'])
 {
 $criterion6 =$_POST['criterion 6'];
 }
 
-$criterion7 ;=NULL
+$criterion7 =NULL;
 if(isset($_POST['criterion 7'])
 {
 $criterion7 =$_POST['criterion 7'];
 }
 
-$criterion8 ;=NULL
+$criterion8 =NULL;
 if(isset($_POST['criterion 8'])
 {
 $criterion8 =$_POST['criterion 8'];
@@ -57,11 +76,59 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-    $create= "CREATE TABLE IF NOT EXISTS `webtech`.`users` 
+    $create= "CREATE TABLE IF NOT EXISTS `webtech`.`presentions` 
     ( `id` INT(30) NOT NULL AUTO_INCREMENT , 
     `name` VARCHAR(50) NOT NULL UNIQUE  , 
-    `zuggriffcod` VARCHAR(50) NOT NULL UNIQUE, 
+    `zuggriffcode` VARCHAR(50) NOT NULL UNIQUE, 
     `creator` VARCHAR(50) NOT NULL 
+    PRIMARY KEY (`id`)) ENGINE = InnoDB";
+
+
+    // use exec() to create table because no results are returned
+    $conn->exec($create);
+    echo "Table users created successfully";
+ }
+catch(PDOException $exception)
+ {
+ echo $create . "<br>" . $exception->getMessage();
+ }
+ $n=5;
+$creator=$_SESSION['username'];
+$zuggriffcode= getName($n);
+
+$statement = $conn->prepare("INSERT INTO presentions (name, zuggriffcode, creator,) VALUES (:name, :lastname, :zuggriffcode, :creator)");
+$statement->execute(array('name' => $name, 'zuggriffcode' => $zuggriffcode, 'creator' => $creator));   
+
+try {
+	$nameuser=$name."users";
+	
+    $create= "CREATE TABLE IF NOT EXISTS `webtech`.`". $nameuser ."` 
+    ( `id` INT(30) NOT NULL AUTO_INCREMENT , 
+    `user` VARCHAR(50) NOT NULL UNIQUE  , 
+    PRIMARY KEY (`id`)) ENGINE = InnoDB";
+
+
+    // use exec() to create table because no results are returned
+    $conn->exec($create);
+    echo "Table users created successfully";
+ }
+catch(PDOException $exception)
+ {
+ echo $create . "<br>" . $exception->getMessage();
+ }
+ 
+try {	
+	
+    $create= "CREATE TABLE IF NOT EXISTS `webtech`.`". $name ."` 
+    ( `id` INT(30) NOT NULL AUTO_INCREMENT , 
+    `".$criterion1."user` int NOT NULL   , 
+	`".$criterion2."user` int NOT NULL   , 
+	`".$criterion3."user` int NOT NULL   , 
+	`".$criterion4."user` int NOT NULL   , 
+	`".$criterion5."user` int NOT NULL   , 
+	`".$criterion6."user` int NOT NULL   , 
+	`".$criterion7."user` int NOT NULL   , 
+	`".$criterion8."user` int NOT NULL   , 
     PRIMARY KEY (`id`)) ENGINE = InnoDB";
 
 
@@ -96,12 +163,11 @@ catch(PDOException $exception)
 
 
 
-
 ?>
 <?php
 include "header.php";
 ?>
-
+<a href="addstudent.php"><button>Add Student to Presentation</button> </a>
 <?php
 include "footer.php";
 ?>
