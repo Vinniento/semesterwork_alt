32a085d1f420f6f5e471a48bf987f9e7df7dc0c3
Christoph Vorlen <christoph.vorlen@stud.fh-campuswien.ac.at>
2019-04-19 14:53:45 +0200

    Änderungen von Daniel

--- index.php
+++ index.php
@@ -8,21 +8,22 @@ include "header.php";
     <div class="col s8 m6 l4 offset-s2 offset-m3">
       <div class="card">
         <div class="card-content indigo lighten-4 center-align">
-        <a href="#" class="myButton">Rate!</a>
-          <!--<button class="btn btn-royal-blue">Rate!</button>-->
-          <!--<a href="#" class="waves-effect waves-light btn-large s12 m6 l6 pulse">Rate!</a>-->
-        </div>
-        <div class="card-content indigo lighten-5">
-          <div class="input-field center">
-            <input id="code" type="text" class="validate">
-            <label for="code">Enter code here!</label>
-            <span class="helper-text" data-error="invalid code" data-success="valid code"></span>
+          <a href="#" class="myButton">Rate!</a>
+          <div class="row no-margin no-margin-bottom">
+            <form class="col l12">
+              <div class="input-field center-align">
+                <input id="code" type="text" class="validate">
+                <label for="code">Enter code here!</label>
+                <span class="helper-text" data-error="invalid code" data-success="valid code"></span>
+              </div>
+            </form>
           </div>
         </div>
       </div>
     </div>
-    <div class="col s4 m4 l4 hide-on-small-only"></div>
+    <div class="col s4 m4 l4 hide-on-small-only indigo darken-4"></div>
   </div>
+  
 
 <?php
 
