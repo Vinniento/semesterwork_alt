782d23dec0441bb6016b98b995d41bc2ec8bf037
Lucas_NB\Lucas01 <lucas@thiess.info>
2019-04-25 20:26:49 +0200

    rate check [unfertig]

--- rate_check.php
+++ rate_check.php
@@ -13,8 +13,8 @@ if (isset($_POST['submit'])) {
     $preperation_team = $_POST['preperation_team'];
     $necessity_participate = $_POST['necessity_participate'];
 
-/*     $additional_feedback_team = $_POST['intro'];
- */
+//    $additional_feedback_team = $_POST['intro'];
+ 
     $articulation = $_POST['articulation'];
     $talking_speed = $_POST['talking_speed'];
     $pronunciation = $_POST['pronunciation'];
@@ -25,8 +25,30 @@ if (isset($_POST['submit'])) {
     $gesture = $_POST['gesture'];
     $preperation_individual = $_POST['preperation_individual'];
    
-/*     $additional_feedback_individual = $_POST['intro'];
- */
+//     $additional_feedback_individual = $_POST['intro'];
+	$code=$_POST['submit_rating_code'];
+	try{
+    //create connection to database
+    $conn = new PDO("mysql:host=localhost; dbname=webtech", "oliver", "nlkj");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	   $query = "SELECT name FROM presentations WHERE rating_code = :rating_code";
+       $statement = $conn->prepare($query);
+       $statement->bindParam(':rating_code', $code);
+       $statement->execute();
+       
+	   $tabelle = $statement->fetch(PDO::FETCH_ASSOC);
+	   $statement = $conn->prepare("INSERT INTO ".tabelle[name]." (name, id_students, rating_code, creator) VALUES (:name, :id_students, :rating_code, :creator)");
+	   $statement->execute(array('name' => $name, 'id_students' => $id_students, 'rating_code' => $rating_code, 'creator' => $creator));
+	
+	
+	
+	
+	}catch (PDOException $exception) {
+    echo $create . "<br>" . $exception->getMessage();
+}
+
+ 
 } else {
 
     header("Loction: index.php");
