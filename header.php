5d0c34f22d205c13f915f46739c1cf38074c0ed7
Christoph Vorlen <christoph.vorlen@stud.fh-campuswien.ac.at>
2019-04-26 21:22:24 +0200

    header angepasst

--- header.php
+++ header.php
@@ -28,12 +28,21 @@ session_start();
         <ul class="sidenav" id="mobile-demo">
           <li><a href="index.php"><i class="material-icons left">home</i>Home</a></li>
           <li><a href="team.html"><i class="material-icons left">people</i>Team</a></li>
-          <li><a href="about.html">About our Project</a></li>
+          <li><a href="about.html"><i class="material-icons left">web</i>About our Project</a></li>
 
           <?php if (isset($_SESSION['username']) && isset($_SESSION['pwd'])) : ?>
             <li><a href="logout.php">Logout</a></li>
 
-          <?php else : ?>
+            <?php if ($_SESSION['isteacher'] == TRUE) : ?>
+              <li><a href="teacher.php"><i class="material-icons left">account_circle</i>Teacher Area</a></li>
+
+
+            <?php else : ?>
+              <li><a href="students.php"><i class="material-icons left">account_circle</i>Student Area</a></li>
+            <?php
+          endif;
+        else :
+          ?>
             <li><a href="login.php">Login</a></li>
           <?php endif; ?>
 
@@ -42,13 +51,13 @@ session_start();
         <ul class="right hide-on-med-and-down">
           <li><a href="index.php"><i class="material-icons left">home</i>Home</a></li>
           <li><a href="team.php"><i class="material-icons left">people</i>Team</a></li>
-          <li><a href="about.php">About our Project</a></li>
+          <li><a href="about.php"><i class="material-icons left">web</i>About our Project</a></li>
 
           <?php if (isset($_SESSION['username']) && isset($_SESSION['pwd'])) : ?>
-            <li><a href="logout.php">Logout</a></li>
+            <li><a href="logout.php" class="right">Logout</a></li>
 
             <?php if ($_SESSION['isteacher'] == TRUE) : ?>
-              <li><a href="teacher.php" class="btn-floating grey darken-4 z-depth-3 center "><i class="material-icons">account_circle</i></a></li>
+              <li><a href="teacher.php" class="btn-floating grey darken-4 z-depth-3 center"><i class="material-icons">account_circle</i></a></li>
 
 
             <?php else : ?>
