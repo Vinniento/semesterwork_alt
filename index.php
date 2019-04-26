fd79595789301272c7996d02c6c5050ed952d121
Christoph Vorlen <christoph.vorlen@stud.fh-campuswien.ac.at>
2019-04-26 21:29:17 +0200

    Rate Button angepasst

--- index.php
+++ index.php
@@ -10,24 +10,22 @@ include "header.php";
 
     <form action="rate.php" method="post" class="col l12">
       <div class="card">
-
         <div class="card-content indigo lighten-4 center-align">
           <div class="row no-margin no-margin-bottom">
-
             <div class="input-field center-align">
-
-              <input id="code" type="text" name = "rating_code" class="validate" required>
-              <label for="code" name = "rating_code">Enter code here!</label>
-              <button type="submit" class="myButton" name="submit_rating_code">Rate!</a>
-
-                <span class="helper-text" data-error="invalid code" data-success="valid code"></span>
+              <input id="code" type="text" name="rating_code" class="validate" required>
+              <label for="code" name="rating_code">Enter code here!</label>
+              <br>
+              <br>
+              <button type="submit" class="myButton" name="submit_rating_code">Rate!</button>
+              <span class="helper-text" data-error="invalid code" data-success="valid code"></span>
             </div>
+          </div>
+        </div>
+      </div>
     </form>
   </div>
-</div>
-</div>
-</div>
-<div class="col s4 m4 l4 hide-on-small-only indigo darken-4"></div>
+  <div class="col s4 m4 l4 hide-on-small-only indigo darken-4"></div>
 </div>
 
 
