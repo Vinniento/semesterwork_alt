e6615e27931343d0ecd45eef23f5eec605241eda
Felix Ullrich <felix.ullrich@stud.fh-campuswien.ac.at>
2019-04-29 15:05:33 +0200

    team weiter, ned das fertige aber halbwegs

--- team.php
+++ team.php
@@ -9,13 +9,42 @@ include "header.php";
     <div class="container">
         <div class="col s8 m6 l4 left-align">
             <div class="card horizontal">
-                <div class="card-image">
-                    <img src="happyface.png">
+                <div class="card-image"><img src="schauer.jpg" height="100"; width="80"; alt="image not available, ask edmin">
+                </div>
+                    <div class="card-stacked">
+                    <p style="font-size: medium; margin-bottom: 0em; margin-top: 0.5em; margin-left: 1em" >Vincent Schauer</p>
+                    <p style="font-size: medium; margin-top: 0.5em; margin-left: 2em">description <br />
+                        description</p>
+                    </div>
+                </div>
+            </div>
+    </div>
+  </div>
+    <div class="row indigo darken-4 no-margin no-margin-bottom">
+        <div class="container">
+            <div class="col s8 m6 l4 left-align">
+                <div class="card horizontal">
+                    <div class="card-image"><img src="jj.jpg" height="100"; width="80"; alt="image not available, ask admin">
+                    </div>
+                    <div class="card-stacked">
+                        <p style="margin-bottom: 0em; font-size: medium; margin-left: 1em">Lucas Thiess</p>
+                        <p style="font-size: small; margin-left: 2em">description <br />
+                            description</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <div class="row indigo darken-4 no-margin no-margin-bottom">
+    <div class="container">
+        <div class="col s8 m6 l4 left-align">
+            <div class="card horizontal">
+                <div class="card-image"><img src="jj.jpg" height="100"; width="80"; alt="image not available, ask admin">
                 </div>
                 <div class="card-stacked">
-                    <p>Daniel Ziegler</p>
-                    <br>
-                    <p>Responsible for the design</p>
+                    <p style="margin-bottom: 0em; font-size: medium; margin-left: 1em">Felix Ullrich</p>
+                    <p style="font-size: small; margin-left: 2em">description <br />
+                        description</p>
                 </div>
             </div>
         </div>
@@ -25,13 +54,12 @@ include "header.php";
     <div class="container">
         <div class="col s8 m6 l4 left-align">
             <div class="card horizontal">
-                <div class="card-image">
-                    <img src="happyface.png">
+                <div class="card-image"><img src="vorlen.jpg" height="100" width="80"; alt="image not available, ask admin">
                 </div>
                 <div class="card-stacked">
-                    <p>Schauer Vincent</p>
-                    <br>
-                    <p></p>
+                    <p style="font-size: medium; margin-bottom: 0em; margin-left: 1em">Christoph Vorlen</p>
+                    <p style="font-size: small; margin-left: 2em">description<br />
+                        description</p>
                 </div>
             </div>
         </div>
@@ -41,45 +69,12 @@ include "header.php";
     <div class="container">
         <div class="col s8 m6 l4 left-align">
             <div class="card horizontal">
-                <div class="card-image">
-                    <img src="happyface.png">
+                <div class="card-image"><img src="ziegler.jpg" height="100" width="80"; alt="image not available, ask admin">
                 </div>
                 <div class="card-stacked">
-                    <p>Vorlen Christoph </p>
-                    <br>
-                    <p></p>
-                </div>
-            </div>
-        </div>
-    </div>
-  </div>
-  <div class="row indigo darken-4 no-margin no-margin-bottom">
-    <div class="container">
-        <div class="col s8 m6 l4 left-align">
-            <div class="card horizontal">
-                <div class="card-image">
-                    <img src="happyface.png">
-                </div>
-                <div class="card-stacked">
-                    <p>Ullrich Felix</p>
-                    <br>
-                    <p></p>
-                </div>
-            </div>
-        </div>
-    </div>
-  </div>
-  <div class="row indigo darken-4 no-margin no-margin-bottom">
-    <div class="container">
-        <div class="col s8 m6 l4 left-align">
-            <div class="card horizontal">
-                <div class="card-image">
-                    <img src="happyface.png">
-                </div>
-                <div class="card-stacked">
-                    <p>Thiess Lucas</p>
-                    <br>
-                    <p></p>
+                    <p style="font-size: medium; margin-bottom: 0em; margin-left: 1em">Daniel Ziegler</p>
+                    <p style="font-size: small; margin-left: 2em">description<br/>
+                        description</p>
                 </div>
             </div>
         </div>
