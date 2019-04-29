edc574e200add1a72e7d679a07fbbf9ba53f2895
Christoph Vorlen <christoph.vorlen@stud.fh-campuswien.ac.at>
2019-04-29 16:52:25 +0200

    Rating erweitert

--- rating.php
+++ rating.php
@@ -1,13 +1,96 @@
 <?php
 include "header.php";
-
 ?>
-
 
 <div id="buttonBar" class="btn-group">
     <button class="button" onclick="changeRatings()">Add Number to Rating</button>
   </div>
 
+
+<?php
+
+$name = $_POST['name']; //hier muss noch der Name der Gruppe rein, die man bewertet hat
+$i=0;
+
+try{
+    //create connection to database
+	   $conn = new PDO("mysql:host=localhost; dbname=webtech", "oliver", "nlkj");
+       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       $tablenumber = $conn->prepare("SELECT COUNT(id) FROM ".$name);
+
+    $introduction=0;
+    $overview=0;
+    $structure=0;
+    $content = 0;
+    $conclusion = 0;
+    $visualization = 0;
+    $work_as_team = 0;
+    $preperation_team =0; 
+    $necessity_participate =0; 
+    $articulation = 0;
+    $talking_speed = 0;
+    $pronounciation = 0;
+    $competence = 0;
+    $presence = 0;
+    $eye_contact =0; 
+    $posture = 0;
+    $gesture =0;
+    $preperation_individual = 0;
+
+for(;i<$tablenumber;$i++) {
+    $query = "SELECT indroduction, overview, structure, content,conclusion,visualization,work_as_team,preperation_team,necessity_participate,articulation,talking_speed,pronounciation,competence,presence,eye_contact,posture,gesture,preperation_individual FROM ".$name." WHERE id = :id";
+       $statement = $conn->prepare($query);
+       $statement->bindParam(':id', $i);
+       $statement->execute();       
+       $tabelle = $statement->fetch(PDO::FETCH_ASSOC);
+       
+
+
+       $introduction+=$tabelle[indroduction];
+       $overview+=$tabelle[overview];
+       $structure+=$tabelle[structure];
+       $content+=$tabelle[content];
+       $conclusion+=$tabelle[conclusion];
+       $visualization+=$tabelle[visualization];
+       $work_as_team+=$tabelle[work_as_team];
+       $preperation_team+=$tabelle[preperation_team];
+       $necessity_participate+=$tabelle[necessity_participate];
+       $articulation+=$tabelle[articulation];
+       $talking_speed+=$tabelle[talking_speed];
+       $pronounciation+=$tabelle[pronounciation];
+       $competence+=$tabelle[competence];
+       $presence+=$tabelle[presence];
+       $eye_contact+=$tabelle[eye_contact]; 
+       $posture+=$tabelle[posture];
+       $gesture+=$tabelle[gesture];
+       $preperation_individual+=$tabelle[preperation_individual];
+}
+$introduction/=$i;
+$overview/=$i;
+$structure/=$i;
+$content/=$i;
+$conclusion/=$i;
+$visualization/=$i;
+$work_as_team/=$i;
+$preperation_team/=$i;
+$necessity_participate/=$i;
+$articulation/=$i;
+$talking_speed/=$i;
+$pronounciation/=$i;
+$competence/=$i;
+$presence/=$i;
+$eye_contact/=$i;
+$posture/=$i;
+$gesture/=$i;
+$preperation_individual/=$i;
+}
+catch (PDOException $exception) 
+	{
+		echo "ERRRRRRROOOOORRRRR";
+    echo  "<br>" . $exception->getMessage();
+	}
+
+?>
 
 
 
@@ -206,13 +289,11 @@ include "header.php";
 
 <script>
 
-
-
-
 function changeRatings() {
       let rating_content = document.getElementById("content_structure");
       rating_content.innerHTML="5.2";
     }
+
 </script>
 
 
