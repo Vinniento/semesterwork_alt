92d14c89b516ab7cb00d4e3ec602abde0f7068c7
Daniel Ziegler <daniel.ziegler@stud.fh-campuswien.ac.at>
2019-05-12 19:42:17 +0200

    in about text eingefügt

--- about.php
+++ about.php
@@ -6,12 +6,18 @@
   <div class="row no-margin no-margin-bottom indigo darken-4">
     <div class="col s1 m3 l2 hide-on-small"></div>
     <div class="col s10 m6 l8 center-align white">
-      <br><br><p class="center">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
-
-Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, <br/>
-            <a href="maps.html">Location</a>
-      </p><br><br>
+      <br><br>
+      <p class="center">
+      The goal of our project was to create a website that allows both students and teachers to give feedback to presentations of their colleagues.
+      </p>
+      <p>Teachers can create their own accounts and add students to groups. After creating a presentation, the students enter a code, to get access to the feedback form. Both, teachers and students will have to fill out the same form, first for the whole group of the presentation and afterwards they can give additional feedback to individuals of the group. The results are then displayed on a different webpage, whereas the left side shows the feedback given by students and the right side the ones given by teachers.
+      Everything will happen anonymously.
+      </p>
+      <p>When feedback was given to an individual person, this person receives an email with a link that sends them back to our website, where they can view their feedback.
+      </p>
+      <p>And here you can see, where the most of the project was created</p>
+        <a href="maps.html">Location</a>
+      </p>
     </div>
     <div class="col s1 m3 l2 hide-on-small"></div>
   </div>
