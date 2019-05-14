d1b0c59804d7990dcb5723c4a1e7e8ae01f16002
Christoph Vorlen <christoph.vorlen@stud.fh-campuswien.ac.at>
2019-05-14 12:10:02 +0200

    no message

--- rating.php
+++ rating.php
@@ -2,21 +2,28 @@
 include "header.php";
 ?>
 
-<div id="buttonBar" class="btn-group">
+ <!-- <div id="buttonBar" class="btn-group">
     <button class="button" onclick="changeRatings()">Add Number to Rating</button>
   </div>
+  
+-->
 
 
 <?php
 
-$name = $_POST['name']; //hier muss noch der Name der Gruppe rein, die man bewertet hat
+error_reporting(0);
+$name = 'testgruppe'; //hier muss noch der Name der Gruppe rein, die man bewertet hat
 $i=0;
 
 try{
     //create connection to database
 	   $conn = new PDO("mysql:host=localhost; dbname=webtech", "oliver", "nlkj");
        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       $tablenumber = $conn->prepare("SELECT COUNT(id) FROM ".$name);
+       $query= "SELECT COUNT('id') FROM testgruppe";
+       $stm = $conn->prepare($query);
+       $stm->execute();
+
+       $tablenumber = $stm->fetchColumn();
 
     $introduction=0;
     $overview=0;
@@ -34,17 +41,34 @@ try{
     $presence = 0;
     $eye_contact =0; 
     $posture = 0;
-    $gesture =0;
+    $gesture = 0;
     $preperation_individual = 0;
 
-for(;i<$tablenumber;$i++) {
+    $introductionT=0;
+    $overviewT=0;
+    $structureT=0;
+    $contentT = 0;
+    $conclusionT = 0;
+    $visualizationT = 0;
+    $work_as_teamT = 0;
+    $preperation_teamT =0; 
+    $necessity_participateT =0; 
+    $articulationT = 0;
+    $talking_speedT = 0;
+    $pronounciationT = 0;
+    $competenceT = 0;
+    $presenceT = 0;
+    $eye_contactT =0; 
+    $postureT = 0;
+    $gestureT = 0;
+    $preperation_individualT = 0;
+
+for($i=0;$i<$tablenumber;$i++) {
     $query = "SELECT indroduction, overview, structure, content,conclusion,visualization,work_as_team,preperation_team,necessity_participate,articulation,talking_speed,pronounciation,competence,presence,eye_contact,posture,gesture,preperation_individual FROM ".$name." WHERE id = :id";
        $statement = $conn->prepare($query);
        $statement->bindParam(':id', $i);
        $statement->execute();       
        $tabelle = $statement->fetch(PDO::FETCH_ASSOC);
-       
-
 
        $introduction+=$tabelle[indroduction];
        $overview+=$tabelle[overview];
@@ -83,6 +107,91 @@ $eye_contact/=$i;
 $posture/=$i;
 $gesture/=$i;
 $preperation_individual/=$i;
+
+
+
+
+
+
+
+
+$introductionT=0;
+    $overviewT=0;
+    $structureT=0;
+    $contentT = 0;
+    $conclusionT = 0;
+    $visualizationT = 0;
+    $work_as_teamT = 0;
+    $preperation_teamT =0; 
+    $necessity_participateT =0; 
+    $articulationT = 0;
+    $talking_speedT = 0;
+    $pronounciationT = 0;
+    $competenceT = 0;
+    $presenceT = 0;
+    $eye_contactT =0; 
+    $postureT = 0;
+    $gestureT = 0;
+    $preperation_individualT = 0;
+    
+    $j=0;
+
+for($i=0;$i<$tablenumber;$i++) {
+
+    $query = "SELECT isteacher FROM ".$name." WHERE id = :id";
+       $statement = $conn->prepare($query);
+       $statement->bindParam(':id', $i);
+       $statement->execute();       
+       $tabelle = $statement->fetch(PDO::FETCH_ASSOC);
+
+    
+    if ($tabelle[isteacher]="1") {
+    $query = "SELECT indroduction, overview, structure, content,conclusion,visualization,work_as_team,preperation_team,necessity_participate,articulation,talking_speed,pronounciation,competence,presence,eye_contact,posture,gesture,preperation_individual FROM ".$name." WHERE id = :id";
+       $statement = $conn->prepare($query);
+       $statement->bindParam(':id', $i);
+       $statement->execute();       
+       $tabelle = $statement->fetch(PDO::FETCH_ASSOC);
+       
+        $j++;
+
+       $introductionT+=$tabelle[indroduction];
+       $overviewT+=$tabelle[overview];
+       $structureT+=$tabelle[structure];
+       $contentT+=$tabelle[content];
+       $conclusionT+=$tabelle[conclusion];
+       $visualizationT+=$tabelle[visualization];
+       $work_as_teamT+=$tabelle[work_as_team];
+       $preperation_teamT+=$tabelle[preperation_team];
+       $necessity_participateT+=$tabelle[necessity_participate];
+       $articulationT+=$tabelle[articulation];
+       $talking_speedT+=$tabelle[talking_speed];
+       $pronounciationT+=$tabelle[pronounciation];
+       $competenceT+=$tabelle[competence];
+       $presenceT+=$tabelle[presence];
+       $eye_contactT+=$tabelle[eye_contact]; 
+       $postureT+=$tabelle[posture];
+       $gestureT+=$tabelle[gesture];
+       $preperation_individualT+=$tabelle[preperation_individual];
+    }
+}
+$introductionT/=$j;
+$overviewT/=$j;
+$structureT/=$j;
+$contentT/=$j;
+$conclusionT/=$j;
+$visualizationT/=$j;
+$work_as_teamT/=$j;
+$preperation_teamT/=$j;
+$necessity_participateT/=$j;
+$articulationT/=$j;
+$talking_speedT/=$j;
+$pronounciationT/=$j;
+$competenceT/=$j;
+$presenceT/=$j;
+$eye_contactT/=$j;
+$postureT/=$j;
+$gestureT/=$j;
+$preperation_individualT/=$j;
 }
 catch (PDOException $exception) 
 	{
@@ -90,6 +199,13 @@ catch (PDOException $exception)
     echo  "<br>" . $exception->getMessage();
 	}
 
+
+
+$content_structure = ($introduction+$overview+$structure+$content+$conclusion)/5;
+$teamwork = ($work_as_team+$preperation_team+$necessity_participate)/3;
+
+$content_structureT = ($introductionT+$overviewT+$structureT+$contentT+$conclusionT)/5;
+$teamworkT = ($work_as_teamT+$preperation_teamT+$necessity_participateT)/3;
 ?>
 
 
@@ -105,7 +221,7 @@ catch (PDOException $exception)
                     <h5><p class="center"><b>Content & Structure</b></p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="content_structure"></span></p></h5>
+                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="content_structure"><?php echo round($content_structure,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -113,7 +229,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Introduction</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="introduction">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="introduction"><?php echo round($introduction,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -121,7 +237,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Gave an Overview?</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="overview">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="overview"><?php echo round($overview,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -129,7 +245,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Structure of the Presentation itself</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="structure_presentation">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="structure_presentation"><?php echo round($structure,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -137,7 +253,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Content</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="content">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="content"><?php echo round($content,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -145,7 +261,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Conclusion</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="conclusion">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="conclusion"><?php echo round($conclusion,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom indigo lighten-3">
@@ -153,7 +269,7 @@ catch (PDOException $exception)
                     <h5><p class="center"><b>Visualization</b></p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="visualization">1</span></p></h5>
+                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="visualization"><?php echo round($visualization,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom indigo lighten-3">
@@ -161,7 +277,7 @@ catch (PDOException $exception)
                     <h5><p class="center"><b>Teamwork</b></p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="teamwork">1</span></p></h5>
+                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="teamwork"><?php echo round($teamwork,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -169,7 +285,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Acted as a Team</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="acted_as_team">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="acted_as_team"><?php echo round($work_as_team,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -177,7 +293,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Preparation by the Team</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="preparation_team">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="preparation_team"><?php echo round($preperation_team,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -185,10 +301,18 @@ catch (PDOException $exception)
                     <h5><p class="center">Audience felt necessity to participate</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="participation">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="participation"><?php echo round($necessity_participate,1); ?></span></p></h5>
                     </div>
                 </div>
             <!--</div>-->
+
+
+
+
+
+
+
+
         </div>
         <div class="col s6 m6 l6 blue">
         <h3 class="center">Teacher</h3><br>
@@ -197,7 +321,7 @@ catch (PDOException $exception)
                     <h5><p class="center"><b>Content & Structure</b></p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="content_structure">1</span></p></h5>
+                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="content_structure"><?php echo round($content_structureT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -205,7 +329,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Introduction</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="introduction">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="introduction"><?php echo round($introductionT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -213,7 +337,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Gave an Overview?</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="overview">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="overview"><?php echo round($overviewT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -221,7 +345,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Structure of the Presentation itself</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="structure_presentation">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="structure_presentation"><?php echo round($structureT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -229,7 +353,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Content</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="content">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="content"><?php echo round($contentT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -237,7 +361,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Conclusion</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="conclusion">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="conclusion"><?php echo round($conclusionT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom indigo lighten-3">
@@ -245,7 +369,7 @@ catch (PDOException $exception)
                     <h5><p class="center"><b>Visualization</b></p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="visualization">1</span></p></h5>
+                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="visualization"><?php echo round($visualizationT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom indigo lighten-3">
@@ -253,7 +377,7 @@ catch (PDOException $exception)
                     <h5><p class="center"><b>Teamwork</b></p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="teamwork">1</span></p></h5>
+                    <h5><p><span class="w3-badge w3-xlarge w3-black w3-padding" id="teamwork"><?php echo round($teamworkT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -261,7 +385,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Acted as a Team</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="acted_as_team">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="acted_as_team"><?php echo round($work_as_teamT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -269,7 +393,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Preparation by the Team</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="preparation_team">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="preparation_team"><?php echo round($preperation_teamT,1); ?></span></p></h5>
                     </div>
                 </div>
                 <div class="row no-margin-bottom">
@@ -277,7 +401,7 @@ catch (PDOException $exception)
                     <h5><p class="center">Audience felt necessity to participate</p></h5>
                     </div>
                     <div class="col s12 m6 l6 center-align">
-                   <h5><p><span class="w3-badge w3-large w3-black" id="participation">1</span></p></h5>
+                   <h5><p><span class="w3-badge w3-large w3-black" id="participation"><?php echo round($necessity_participateT,1); ?></span></p></h5>
                     </div>
                 </div>
     </div>
@@ -286,7 +410,7 @@ catch (PDOException $exception)
 
 
 
-
+<!--
 <script>
 
 function changeRatings() {
@@ -295,7 +419,7 @@ function changeRatings() {
     }
 
 </script>
-
+-->
 
 <?php
 
